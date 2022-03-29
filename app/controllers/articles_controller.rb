35d742efbab2252abf34061ca2f6fb31bb349614
Eplasbaba <adwaith2241@gmail.com>
2022-03-29 13:12:33 +0800

    flash messages and validation errors added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,15 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     def new
-
+        @article = Article.new
     end
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        redirect_to article_path(@article)
+        if @article.save
+            flash[:notice] = "Article was created and saved!!"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
 end
